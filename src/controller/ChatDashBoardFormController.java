cc41ed2a7e8a9ea96d0f12daafd364de0b9ef502
Thilina342 <thilinabandar67@gmail.com>
2022-08-11 18:58:24 +0530

    Create Login

--- src/controller/ChatDashBoardFormController.java
+++ src/controller/ChatDashBoardFormController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class ChatDashBoardFormController {
+    public AnchorPane chatRoomPane;
+    public TextArea chatBoard;
+    public TextField txtSend;
+    public Button btnSend;
+
+
+
+
+
+    public void onClickSendMassage(ActionEvent actionEvent) {
+    }
+
+    public void onClickBtnSend(ActionEvent actionEvent) {
+    }
 }
--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,16 +1,38 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public AnchorPane loginPane;
     public TextField txtuserName;
     public Button btnLogin;
 
-    public void onClickLogin(ActionEvent actionEvent) {
+    static String userName;
+
+    public void onClickLogin(ActionEvent actionEvent) throws IOException {
+        userName=txtuserName.getText();
+        if (userName.equals("")) {
+            new Alert(Alert.AlertType.WARNING, "Waradiii Buruwooooooooo!..").showAndWait();
+        }else {
+            Stage stage = (Stage) txtuserName.getScene().getWindow();
+            stage.close();
+            Stage stage1 = new Stage();
+            stage1.setScene(new Scene(FXMLLoader.load(getClass().getResource("../views/ChatDashBoardForm.fxml"))));
+            stage1.setResizable(false);
+            stage1.setTitle(userName);
+            stage1.centerOnScreen();
+            stage1.show();
+        }
+
     }
 
     public void onclickUseName(ActionEvent actionEvent) {
