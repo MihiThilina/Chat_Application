3db34fbdb3e924f8cb603fea398073819d34b98d
Thilina342 <thilinabandar67@gmail.com>
2022-08-11 11:08:49 +0530

    First commit

---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public AnchorPane loginPane;
+    public TextField txtuserName;
+    public Button btnLogin;
+
+    public void onClickLogin(ActionEvent actionEvent) {
+    }
+
+    public void onclickUseName(ActionEvent actionEvent) {
+    }
+}
